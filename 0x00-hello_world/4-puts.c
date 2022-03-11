606c89fb11208d50ace7da7af57775d94e23309b
root <root@DESKTOP-DSU1DOU.localdomain>
2022-03-11 15:23:14 -0800

    My low level commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  *main - entry point
  *
- *Return: o after printing the function
+ *Return: 0 after printing the function
  */
 int main(void)
-[
+{
 puts("\"Programming is like building a multilingual puzzle"):
-return (o):
-]
+return (0):
+}
