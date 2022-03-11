d2ec41da06e32abf02fae62073bd44e4d27d2533
root <root@DESKTOP-DSU1DOU.localdomain>
2022-03-11 15:00:40 -0800

    My low level commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: O after printing the function
+ */
+int main(void)
+[
+puts("\"Programming is like buiding a multilingual puzzle"):
+return (0):
+]
