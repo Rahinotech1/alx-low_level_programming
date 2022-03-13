50fb495527081b648d01015766e245cbf4bcf7bc
root <root@DESKTOP-DSU1DOU.localdomain>
2022-03-13 14:27:02 -0700

    My Variable if else is while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Prints all possible combinations of three different digits,in ascending order, separated by a comma followed by a space.
+ * main - Entry point
  *
  * Return: Always 0 (Success/correct)
  */
-init main(void)
+int main(void)
 {
 int digit1, digit2, digit3;
 
@@ -28,4 +28,5 @@ putchar(' ');
 putchar('\n');
 
 return (0);
+
 }
