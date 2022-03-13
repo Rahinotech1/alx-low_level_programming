592d1fe3de3fbaa13a2f68e62ae2393562bf117f
root <root@DESKTOP-DSU1DOU.localdomain>
2022-03-13 07:18:14 -0700

    My Variable if else is while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet = 'a' 'A' ; alphabet <= 'z' 'Z' ; aphabet++)
+{
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+
+}
