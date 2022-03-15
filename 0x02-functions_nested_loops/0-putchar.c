f85b83c25378d3d1b2b34b61651dd88784c6b935
root <root@DESKTOP-DSU1DOU.localdomain>
2022-03-15 10:30:26 -0700

    My second low level programming commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+*/
+
+int main(void)
+{
+
+char *sh = "_putchar";
+
+while (*sh)
+{
+_putchar(*sh);
+sh++;
+}
+_putchar('\n');
+
+return (0);
+
+}
