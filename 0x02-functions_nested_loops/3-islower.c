5f10543e81aaf9d9a4145c8be4baa32ec0db1111
root <root@DESKTOP-DSU1DOU.localdomain>
2022-03-15 15:26:22 -0700

    My second low level programming commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _islower - a function that checks for lowercase character
- * Description: single letter input
- * Return: Always 0 (Success/correct)
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
  */
 
 int _islower(int c)
