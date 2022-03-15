db8cacdb8a57421c2f5a2043311669d9f91d2d1b
root <root@DESKTOP-DSU1DOU.localdomain>
2022-03-15 14:47:14 -0700

    My second low level programming commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * int _isalpha - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
+
