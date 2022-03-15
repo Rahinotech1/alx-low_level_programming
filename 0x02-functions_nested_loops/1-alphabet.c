3949b2a82687aeef8ed4bceaedb5336d20000aea
root <root@DESKTOP-DSU1DOU.localdomain>
2022-03-15 14:04:40 -0700

    My second low level programming commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+void print_alphabet(void)
+{
+char ch;
+
+ch = 'a';
+
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
