fc13f7cee2abc1762417b0d19b363da530587cd1
root <root@DESKTOP-DSU1DOU.localdomain>
2022-03-15 16:11:35 -0700

    My second low level programming commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- *jack_bauer - function that prints every minute of the day, 24 hour clock, h = hour, m = minutes, 10 allows second digit to rotate for loop breaks before passing 24:00
+ *jack_bauer - function that prints every minute of the day
  *
  *Return: 24 hour clock line by line
  */
+
 void jack_bauer(void)
 {
 int h, m;
