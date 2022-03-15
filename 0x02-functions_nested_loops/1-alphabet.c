935025e098c8793811231f2fa370bee14f6920a8
root <root@DESKTOP-DSU1DOU.localdomain>
2022-03-15 12:19:32 -0700

    My second low level programming commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - Entry point
+ * main - check the code
  *
  * Return: Always 0 (Success/correct)
  */
 
-void print_alphabet(void)
+int main(void)
 {
-
 int n;
 
 for (n = 'a'; n <= 'z'; n++)
