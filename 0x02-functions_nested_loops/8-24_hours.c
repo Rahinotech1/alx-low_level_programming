7bc460a12b6cb0aa6902b089158fa07e917f8909
root <root@DESKTOP-DSU1DOU.localdomain>
2022-03-15 16:02:19 -0700

    My second low level programming commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -18,8 +18,7 @@ _putchar((h / 10) + '0');
 _putchar((h % 10) + '0');
 _putchar(':');
 _putchar((m / 10) + '0');
-_putchar((m % 10) + '0');																					
-_putchar('\n')
+_putchar((m % 10) + '0');												_putchar('\n');
 }
 }
 }
