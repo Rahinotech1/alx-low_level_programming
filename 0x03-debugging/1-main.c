f2effc494866d6930f599cc928ed9f0ed2c76367
root <root@DESKTOP-DSU1DOU.localdomain>
2022-03-17 06:44:45 -0700

    My debugging commit task

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,11 +12,11 @@ printf("Infinite loop incoming :(\n");
 i = 0;
 
 /*
-*while (i < 10);
+*while (i < 10)
 *{
 *Infinite loop - No increase of variable
 *putchar(i);
-*}
+}
 */
 printf("Infinite loop avoided! \\o/\n");
 return (0);
