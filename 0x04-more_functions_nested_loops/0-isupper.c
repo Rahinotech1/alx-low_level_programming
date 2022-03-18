551f11be179ec845a99d96b23fd6cfc884091839
root <root@DESKTOP-DSU1DOU.localdomain>
2022-03-18 07:52:03 -0700

    My 0x04-more_functionscommit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isuper - Evaluate if letter is uppercase.
+ * @c: print int
+ * Return: Always 0.
+ */
+
+int _isuper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
