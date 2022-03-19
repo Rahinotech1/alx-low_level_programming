c49987814dae413d3cb2ae3596887b839bd1031b
root <root@DESKTOP-DSU1DOU.localdomain>
2022-03-18 19:27:03 -0700

    My 0x04-more_functionscommit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,23 +9,28 @@
 
 void print_square(int size)
 {
-int row, column;
 
-if ( size <= 0)
+int i;
+int k;
+
+if (size > 0)
 {
-putchar('\n');
+for (i = 0; i < size; i++)
+{
+
+for (k = 0; k < size; k++)
+{
+putchar (35);
 }
+putchar ('\n');
+
+}
+
+}
+
 else
 {
-for (row = 1; row <= size; row++)
-{
-for (column = 1; column <= size; column++)
-{
-putchar('#');
-}
 putchar('\n');
 }
 
 }
-
-
