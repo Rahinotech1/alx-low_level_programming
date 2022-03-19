e762a52757875aecf4d9314e5877310de9d1b7ed
root <root@DESKTOP-DSU1DOU.localdomain>
2022-03-19 11:00:21 -0700

    My 0x04-more_functionscommit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,47 +1,29 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
- * Return: void
+ * Description: prints FizzBuzz
+ * Return: Always 0
  */
 
 int main(void)
 {
+int n;
 
-int p = 100;
-int i;
-
-i = 1;
-while (i <= p)
+for (n = 1; n <= 100; n++)
 {
-if (i % 3 == 0 && i % 5 == 0)
-{
+if ((n % 3 == 0) && (n % 5 == 0))
 printf("FizzBuzz");
-}
-
-else if (i % 3 == 0)
-{
+else if (n % 5 == 0)
+printf("Buzz");
+else if (n % 3 == 0)
 printf("Fizz");
-}
-
-else if (i % 5 == 0)
-{
-if (i < p)
-printf("Buzz");
-
 else
-printf("Buzz");
-}
+printf("%i", n);
 
+if (n < 100)
+printf(" ");
 else
-{
-printf("%i" , i);
-}
-
-i++;
-
-
-}
 printf("\n");
+}
 return (0);
 }
