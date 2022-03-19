42d62003cc7ab3df622b9af50470e051a2ef1453
root <root@DESKTOP-DSU1DOU.localdomain>
2022-03-18 19:12:48 -0700

    My 0x04-more_functionscommit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_square - Entry point
+ * Description: Prints square
+ * @size: size of square
+ * Return: void
+ */
+
+void print_square(int size)
+{
+int row, column;
+
+if ( size <= 0)
+{
+putchar('\n');
+}
+else
+{
+for (row = 1; row <= size; row++)
+{
+for (column = 1; column <= size; column++)
+{
+putchar('#');
+}
+putchar('\n');
+}
+}
+
