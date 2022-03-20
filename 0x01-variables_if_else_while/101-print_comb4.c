3d29871b1fe5edfde9f63c584a93574ed26e9526
root <root@DESKTOP-DSU1DOU.localdomain>
2022-03-19 20:55:10 -0700

    My variables if else while commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - prints all possible different combinations of three digits
  *
- * Return: Always 0 (Success/correct)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-int digit1, digit2, digit3;
+int n, m, l;
 
-for (digit1 = 0; digit1 < 8; digit1++)
+for (n = 48; n < 58; n++)
 {
-for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+for (m = 49; m < 58; m++)
 {
-putchar((digit1 % 10) + '0');
-putchar((digit2 % 10) + '0');
-putchar((digit3 % 10) + '0');
-
-if (digit1 == 7 && digit2 == 8 && digit3 == 9)
-continue;
-
+for (l = 50; l < 58; l++)
+{
+if (l > m && m > n)
+{
+{
+putchar(n);
+putchar(m);
+putchar(l);
+if (n !=55 || m !=56)
+{
 putchar(',');
 putchar(' ');
-
 }
 }
 }
-
+}
 putchar('\n');
-
 return (0);
-
 }
+
