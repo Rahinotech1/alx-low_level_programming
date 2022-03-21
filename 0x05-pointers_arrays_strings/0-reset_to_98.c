6f9ee010d32af14e67c2859d2e4603ce448c8b28
root <root@DESKTOP-DSU1DOU.localdomain>
2022-03-20 23:03:32 -0700

    My pointers arrays strings commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * reset_to_98 - resets value of pointer to 98
+ * reset_to_98 - reset value of pointer to 98
  * @n: pointer to reset to 98
- * 
  * Return: Always 0
  */
 void reset_to_98(int *n)
