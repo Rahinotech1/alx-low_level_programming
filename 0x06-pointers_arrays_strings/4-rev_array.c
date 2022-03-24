b085369f63c8be806894d0afc40d9864c820aff9
root <root@DESKTOP-DSU1DOU.localdomain>
2022-03-24 05:14:02 -0700

    My pointers arrays strings commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * reverse_array - reverses the contents of an array
+ * @a: integer array to be reversed
+ * @n: length of array a
+ * Return: Always 0
+ */
+void reverse_array(int *a, int n)
+{
+int i = 0, t;
+
+n = n - 1;
+while (i < n)
+{
+t = *(a + i);
+*(a + i) = *(a + n);
+*(a + n) = t;
+i++;
+n--;
+}
+}
