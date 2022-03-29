3f54ccf5fe3955f06e17b8917a5a40865d2409d9
root <root@DESKTOP-DSU1DOU.localdomain>
2022-03-28 22:49:56 -0700

    My commit on 0x07 pointers arrays strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _memset - fills the first byte of memory with a constant byte
+ * _memset - fills memory with a constant byte
  * @s: memory space to be filled	
  * @b: byte to fill with
  * @n: number of spaces to fill
- * Return: Aways 0
+ *
+ * Return: pointer to memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
