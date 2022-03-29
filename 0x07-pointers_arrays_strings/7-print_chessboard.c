55e08addd0e149f305f758c7433095a654e806b6
root <root@DESKTOP-DSU1DOU.localdomain>
2022-03-29 15:18:59 -0700

    My commit on 0x07 pointers arrays strings

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,8 +1,9 @@
+#include <stdio.h>
 #include "main.h"
-
 /**
  * print_chessboard - prints the chessboard
  * @a: pointer to pieces to print
+ *
  * Return: Always 0
  */
 void print_chessboard(char (*a)[8])
@@ -13,8 +14,8 @@ for (i = 0; i < 8; i++)
 {
 for (j = 0; j < 8; j++)
 {
-putchar(a[i][j]);
+_putchar (a[i][j]);
 }
-putchar('\n');
+_putchar('\n');
 }
 }
