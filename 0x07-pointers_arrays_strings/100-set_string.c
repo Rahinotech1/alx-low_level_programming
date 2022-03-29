8d95e4fe27ca3251d4a711d7cb73b7f87fe2243a
root <root@DESKTOP-DSU1DOU.localdomain>
2022-03-29 15:38:12 -0700

    My commit on 0x07 pointers arrays strings

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to change
+ * @to: string to change pointer to
+ * Return: Always 0
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
