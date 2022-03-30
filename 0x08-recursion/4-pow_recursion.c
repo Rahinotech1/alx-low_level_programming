5cf11a330a4aebffcc732117ed4a3e3dc98b3c0e
root <root@DESKTOP-DSU1DOU.localdomain>
2022-03-30 12:44:40 -0700

    Recursion commit

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,8 +2,8 @@
 #include "main.h"
 /**
  * _pow_recursion - Fuctions that returns the value of x raised to the power of y
- * @x: value
- * @y: power
+ * @x: base number
+ * @y: exponent
  * Return: Always 0
  */
 int _pow_recursion(int x, int y)
