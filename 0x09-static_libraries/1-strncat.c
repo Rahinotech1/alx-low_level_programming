26c2cdaa9413ee89ca2ca8ca073c3c7644b0c4d6
root <root@DESKTOP-DSU1DOU.localdomain>
2022-04-02 13:39:09 -0700

    My commit on static libraries

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strncat - Function that concatenates src to the dest string
+ * @dest: string that concatenates two strings by src 
+ * @src: string that concatenates two strings by dest
+ * @n: numbers that contains more bytes
+ * Return: Always 0
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i, j;
+
+i = j = 0;
+while (*(dest + i))
+i++;
+while (j < n && *(src + j))
+{
+*(dest + i) = *(src + j);
+i++;
+j++;
+}
+if (j < n)
+*(dest + i) = *(src + j);
+return (dest);
+}
