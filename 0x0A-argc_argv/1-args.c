e4042fff8d5963f7fe4d3cfbb53fbf95d7f08da5
root <root@DESKTOP-DSU1DOU.localdomain>
2022-04-03 07:14:47 -0700

    Commits on Argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - Program that prints number of arguments
+ * @argc: number of arguments
+ * @argv: array pointing to argument
+ * Return: Always 0
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+int i;
+
+for (i = 0; i < argc; i++)
+{
+}
+printf("%d\n", i - 1);
+return (0);
+}
