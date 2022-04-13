8cd109ab26946d84061bff852aada8d76bce3ff3
root <root@DESKTOP-DSU1DOU.localdomain>
2022-04-12 18:30:18 -0700

    Structures and typedef commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,20 +4,20 @@
 /**
  * print_dog - Write a function that prints a struct dog
  * @d: struct name
- * Return: 0
+ * Return: Always 0
  */
 void print_dog(struct dog *d)
 {
-if (d == NULL)
-return;
-if (d->name == NULL)
-d->name = "(nil)";
-if (d->age <= 0)
-printf("Age: (nil)\n");
-if (d->owner == NULL)
-d->owner = "(nil)";
-
+if (d != NULL)
+{
+if (d->name != NULL)
 printf("Name: %s\n", d->name);
+else
+printf("Name: (nil)\n");
 printf("Age: %.6f\n", d->age);
+if (d->owner != NULL)
 printf("Owner: %s\n", d->owner);
+else
+printf("Owner: (nil)\n");
+}
 }
