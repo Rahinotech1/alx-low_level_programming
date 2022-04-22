fbbd5a529e25c4e09a787c6afdc61172a2a4f97b
root <root@DESKTOP-DSU1DOU.localdomain>
2022-04-21 19:02:57 -0700

    Commit on singly linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,6 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 /**
+ * _strlen - finds the length of a string
+ * @str: string to find the length of
+ *  
+ * Return: length of string
+ */
+unsigned int _strlen(char *str)
+{
+unsigned int i;
+
+for (i = 0; str[i]; i++)
+;
+return (i);
+}
+
+/**
  * add_node - Entry Point
  * @head: input
  * @str: string
