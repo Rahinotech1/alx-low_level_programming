0be72ff62735b6635d9d36b0074cd00460785f71
root <root@DESKTOP-DSU1DOU.localdomain>
2022-04-21 18:58:23 -0700

    Commit on singly linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,24 +7,21 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-int i, count = 0;
 list_t *new;
 
-if (head == NULL)
+if (str == NULL)
 return (NULL);
-
 new = malloc(sizeof(list_t));
 if (new == NULL)
 return (NULL);
-
-for (i = 0; str[i] != '\0'; i++)
-count++;
-
-new->len = i;
 new->str = strdup(str);
+if (new->str == NULL)
+{
+free(new);
+return (NULL);
+}
+new->len = _strlen(new->str);
 new->next = *head;
-
 *head = new;
-
 return (new);
 }
