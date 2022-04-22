f97e702f6b58cef08741ee94b4b1231b2305f6c7
root <root@DESKTOP-DSU1DOU.localdomain>
2022-04-21 19:11:44 -0700

    Commit on singly linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,11 +1,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
-
 /**
  * _strlen - finds the length of a string
  * @str: string to find the length of
- * 
  * Return: length of string
  */
 unsigned int _strlen(char *str)
@@ -21,7 +19,6 @@ return (i);
  * add_node_end - adds a new node to the end of linked list
  * @head: double pointer to a linked list
  * @str: string to add to the new node
- *   
  * Return: pointer to the new node
  */
 list_t *add_node_end(list_t **head, const char *str)
