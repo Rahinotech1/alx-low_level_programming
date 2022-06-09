1a3a4507be2277a224e1f5f1d259868760d28f00
Rahinotech1 <Rahinotech1720@gmail.com>
2022-06-09 10:42:09 -0700

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -6,12 +6,12 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-size_t count = 0;
+size_t tot = 0;
 
-while (h != NULL)
+while (h)
 {
+tot++;
 h = h->next;
-count++;
 }
-return (count);
+return (tot);
 }
