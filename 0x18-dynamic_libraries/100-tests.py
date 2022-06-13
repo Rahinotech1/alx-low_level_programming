73f794a17d583d6cee3e81908cb32d353c2c3ee6
Rahinotech1 <Rahinotech1720@gmail.com>
2022-06-13 13:35:38 -0700

    Call C from Python

---
+++ 0x18-dynamic_libraries/100-tests.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+import ctypes
+
+so_file = ctypes.CDLL('./100-operations.so')
+100-operations = CDLL(so_file)
+
+print(type(100-functions))
+print(100-functions.square(10))
+
+print("Done")
+
