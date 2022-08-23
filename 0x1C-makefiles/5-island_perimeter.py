49bea2d7f9545b100d57b511577737f4eb460994
Rahinotech1 <99316887+Rahinotech1@users.noreply.github.com>
2022-08-23 04:15:30 -0700

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,39 +1,26 @@
 #!/usr/bin/python3
-"""
-module for interview prep, tried more efficient solution but too much
-"""
+"""Defines an island perimeter measuring function."""
 
 
 def island_perimeter(grid):
-    """ gets perimeter of island represented as grid
+    """Return the perimiter of an island.
+    The grid represents water by 0 and land by 1.
+    Args:
+        grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
     """
-    if not isinstance(grid, list):
-        return 0
-    island_p = 0
-    for row_i, row in enumerate(grid):
-        if not isinstance(row, list):
-            return 0
-        for col_i, block in enumerate(row):
-            if type(block) is not int:
-                return 0
-            if block == 1:
-                island_p += get_block_sum(grid, row_i, col_i)
-    return island_p
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-
-def get_block_sum(grid, r_i, c_i):
-    """
-    gets the perimeter of a block
-    """
-    next_block = None
-    block_p = 4
-    try:
-        r_block = grid[r_i][c_i + 1]
-        l_block = grid[r_i][c_i - 1]
-        t_block = grid[r_i - 1][c_i]
-        b_block = grid[r_i + 1][c_i]
-    except:
-        raise ValueError("ISLAND IS NOT SURROUNDED BY WATER")
-
-    block_p += -1 * (r_block + l_block + t_block + b_block)
-    return block_p
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
